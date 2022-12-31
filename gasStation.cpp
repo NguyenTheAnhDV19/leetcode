e97dbb4f42c3911f30b3f99a5b0ce08382d9e52d
BoyAnh <90632032+NguyenTheAnhDV19@users.noreply.github.com>
2022-12-31 22:32:26 +0700

    Create gasStation.cpp

---
+++ gasStation.cpp
@@ -0,0 +1,50 @@
+#include<iostream>
+#include<map>
+#include<string>
+#include<algorithm>
+#include<vector>
+
+class Solution {
+private:
+    bool checkCost(int position, int cost[], int gas[]);
+
+public:
+    int gasStation(int cost[],int gas[])
+    {
+        int position =0;
+        for( position; position<3;position++)
+        {
+            if(checkCost(position,cost,gas) == true) return position+1;
+        }
+    return -1;
+    }
+};
+
+bool Solution::checkCost(int position, int cost[], int gas[])
+{
+    int remain = 0; int j=-1; int rePos = position;
+    for(int i=0 ;i<=3;i++)
+    {
+        if(rePos <=2)
+        {
+            remain += (gas[rePos] - cost[rePos]);
+        }else{
+            remain += (gas[rePos-2] - cost[rePos-2]);
+        }
+        std::cout<<rePos<<std::endl;
+        if(remain<=0) break;
+        rePos++; j++;
+    }
+    std::cout<<"endl"<<std::endl;
+    if(rePos - 3 == position) return true;
+    return false;
+}
+
+int main()
+{
+    int cost[3] = {1,2,3};
+    int gas[3] = {2,1,6};
+    Solution solution;
+    std::cout<<solution.gasStation(cost,gas)<<std::endl;
+    return 0;
+}
